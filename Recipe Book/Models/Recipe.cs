b3ef44158751c93ad79cec0e27250eed178ea81f
Michael <michael.davis4@outlook.com>
2019-03-05 21:36:12 -0800

    Created file for new Recipe class.

---
+++ Recipe Book/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Book.Models
+{
+    class Recipe
+    {
+        private String name;
+        private short rating;
+        private String lastMade;
+
+        public Recipe()
+        {
+            this.name = "";
+            this.rating = 0;
+            this.lastMade = "";
+        }
+    }
+}
