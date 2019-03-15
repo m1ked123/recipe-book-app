33aa1c6ef6ac31045327d7df3b024193ec24291b
Michael <michael.davis4@outlook.com>
2019-03-14 21:01:29 -0700

    Added fields and constructors to basic Recipe implementation.

--- Recipe Book/Models/Recipe.cs
+++ Recipe Book/Models/Recipe.cs
@@ -8,15 +8,101 @@ namespace Recipe_Book.Models
 {
     class Recipe
     {
+
         private String name;
+        private long id;
         private short rating;
         private String lastMade;
+
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public short Rating
+        {
+            get
+            {
+                return this.rating;
+            }
+            set
+            {
+                if (value >= 0 && value < 6)
+                {
+                    this.rating = value;
+                }
+            }
+        }
+
+        public String LastMade
+        {
+            get
+            {
+                return this.lastMade;
+            }
+            set
+            {
+                this.lastMade = value;
+            }
+        }
+
+        public long ID
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
 
         public Recipe()
         {
             this.name = "";
+            this.id = -1;
             this.rating = 0;
             this.lastMade = "";
+        }
+
+        public Recipe(String name)
+        {
+            this.name = name;
+            this.id = -1;
+            this.rating = 0;
+            this.lastMade = "";
+        }
+
+        public Recipe(String name, long id)
+        {
+            this.name = name;
+            this.id = id;
+            this.rating = 0;
+            this.lastMade = "";
+        }
+
+        public Recipe(String name, long id, short rating)
+        {
+            this.name = name;
+            this.id = id;
+            this.rating = rating;
+            this.lastMade = "";
+        }
+
+        public Recipe(String name, long id, short rating, String lastMade)
+        {
+            this.name = name;
+            this.id = id;
+            this.rating = rating;
+            this.lastMade = lastMade;
         }
     }
 }
