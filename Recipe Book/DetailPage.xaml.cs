86d3624463999ea0824d8b18a1ed0f185425004e
Michael <michael.davis4@outlook.com>
2019-05-18 23:03:38 -0700

    Made it so recipes can be edited and removed in the compact view.

--- Recipe Book/DetailPage.xaml.cs
+++ Recipe Book/DetailPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Recipe_Book.Models;
 using Recipe_Book.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
@@ -64,6 +67,71 @@ namespace Recipe_Book
             // Make sure we are using the "drill out" animation in this transition.
             e.Handled = true;
             Frame.GoBack(new DrillInNavigationTransitionInfo());
+        }
+
+        private void editSelectedRecipe(object sender, RoutedEventArgs e)
+        {
+            recipes.setEditing(true);
+            Frame.Navigate((typeof(RecipeForm)), recipes);
+        }
+
+        private void deleteSelectedRecipe(object sender, RoutedEventArgs e)
+        {
+            Recipe recipeToDelete = recipes.getSelected();
+            tryDeleteRecipe(recipeToDelete);
+        }
+
+        private async void tryDeleteRecipe(Recipe recipeToDelete)
+        {
+            ContentDialog deleteConfirmationDialog = new ContentDialog
+            {
+                Title = "Permenantly delete recipe?",
+                Content = "If you delete this recipe, you won't be" +
+                " able to recover it. Are you Sure you want to delete" +
+                " it?",
+                PrimaryButtonText = "Delete",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await deleteConfirmationDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                int oldIndex = recipes.getRecipeList().IndexOf(recipeToDelete);
+
+                int currSelectedRecipe = recipes.getSelectedIndex(); ;
+
+                if (currSelectedRecipe == oldIndex && currSelectedRecipe > 0)
+                {
+                    recipes.setSelected(currSelectedRecipe - 1);
+                }
+                else if (currSelectedRecipe == oldIndex && currSelectedRecipe == 0)
+                {
+                    recipes.setSelected(0);
+                }
+                else
+                {
+                    recipes.setSelected(currSelectedRecipe);
+                }
+                recipes.removeRecipe(recipeToDelete);
+
+                IList<PageStackEntry> backStack = Frame.BackStack;
+                int backStackCount = backStack.Count;
+                if (backStackCount > 0)
+                {
+                    PageStackEntry masterPageEntry = backStack[backStackCount - 1];
+                    backStack.RemoveAt(backStackCount - 1);
+
+                    PageStackEntry modifiedEntry = new PageStackEntry(
+                        masterPageEntry.SourcePageType,
+                        recipes.getSelectedIndex(),
+                        masterPageEntry.NavigationTransitionInfo
+                        );
+
+                    backStack.Add(modifiedEntry);
+                }
+                Frame.GoBack();
+            }
         }
     }
 }
--- Recipe Book/MainPage.xaml.cs
+++ Recipe Book/MainPage.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using Recipe_Book.Models;
 using Recipe_Book.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -57,7 +45,6 @@ namespace Recipe_Book
                 index = (int)e.Parameter;
             }
 
-            Debug.WriteLine("initial index: " + index);
             this.recipeListView.SelectedIndex = index;
             this.recipes.setSelected(index);
 
