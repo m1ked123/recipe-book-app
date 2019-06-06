618d009ab81a4e611ed75b88179bfd59f391af6a
Michael <michael.davis4@outlook.com>
2019-06-05 21:57:48 -0700

    Added basic setup for emptying a recipe book.

--- Recipe Book/SettingsPage.xaml.cs
+++ Recipe Book/SettingsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Recipe_Book.Utils;
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -25,14 +27,35 @@ namespace Recipe_Book
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base. OnNavigatedTo(e);
-
-            this.unitsOfMeasureList.ItemsSource = units;
         }
 
-        private void addUnitOfMeasure(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void tryEmptyBook(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            this.units.Add("New unit: " + n);
-            n++;
+            ContentDialog emptyConfirmationDialog = new ContentDialog
+            {
+                Title = "Permenantly empty recipe book?",
+                Content = "If you empty your recipe book, you won't be" +
+                " able to recover the recipes in it. Are you Sure you " +
+                "want to empty your recipe book?",
+                PrimaryButtonText = "Empty",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await emptyConfirmationDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+
+                // Delete recipes in database
+                // Create a new recipe list object
+                // Set the empty button to be disabled
+                Debug.WriteLine("Recipe book emptied");
+            }
+        }
+
+        private void goBack(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Frame.GoBack();
         }
     }
 }
