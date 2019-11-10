316197fa708f4557159f98e09efe41990c5ec4f6
Michael <michael.davis4@outlook.com>
2019-11-09 20:51:32 -0800

    Reduced the amount of color and made it so the frame area extends into the title bar area.

--- Recipe Book/App.xaml.cs
+++ Recipe Book/App.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.ObjectModel;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Storage;
 using Windows.UI;
 using Windows.UI.ViewManagement;
@@ -106,17 +107,25 @@ namespace Recipe_Book
                 Window.Current.Activate();
             }
 
+            //var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
             var titleBar = ApplicationView.GetForCurrentView().TitleBar;
 
-            // Set active window colors
-            titleBar.ForegroundColor = Colors.White;
-            titleBar.BackgroundColor = Color.FromArgb(255, 0, 76, 64);
-            titleBar.ButtonForegroundColor = Colors.White;
-            titleBar.ButtonBackgroundColor = Color.FromArgb(255, 0, 76, 64);
-            titleBar.ButtonHoverForegroundColor = Colors.White;
-            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 0, 121, 107);
+            titleBar.ButtonForegroundColor = Colors.Black;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonHoverForegroundColor = Colors.Black;
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 72, 169, 153);
             titleBar.ButtonPressedForegroundColor = Colors.White;
-            titleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 0, 37, 26);
+            titleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 0, 76, 64);
+
+            // Set active window colors
+            /*
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.BackgroundColor = Color.FromArgb(255, 0, 121, 107);
+            
+            */
         }
 
         /// <summary>
